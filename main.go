6727c1e660aa33207701c9d1f76c01a41736219e
Donal Byrne <byrnedo@tcd.ie>
2016-07-06 14:44:40 +0200

    Replaced missed Action funcs

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ func main() {
 			Name:    "up",
 			Aliases: []string{},
 			Usage:   "Create then run or update containers",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				//first get settings
 				settings := getSettings()
 				settings.LaunchSignalWatcher()
@@ -88,6 +88,8 @@ func main() {
 					os.Exit(1)
 				}
 
+				return nil
+
 			},
 			Flags: []cli.Flag{
 				cli.BoolFlag{
@@ -101,7 +103,7 @@ func main() {
 			Name:    "create",
 			Aliases: []string{},
 			Usage:   "Create containers, but don't run them",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.create") {
 					os.Exit(1)
@@ -116,14 +118,14 @@ func main() {
 				if !settings.RunHook("after.create") {
 					os.Exit(1)
 				}
-
+				return nil
 			},
 		},
 		{
 			Name:    "start",
 			Aliases: []string{},
 			Usage:   "Start stopped containers",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				settings.LaunchSignalWatcher()
 				if !settings.RunHook("before.start") {
@@ -139,6 +141,8 @@ func main() {
 				if !settings.RunHook("after.start") {
 					os.Exit(1)
 				}
+
+				return nil
 			},
 			Flags: []cli.Flag{
 				cli.BoolFlag{
@@ -153,7 +157,7 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Number of instances to run of container",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.scale") {
 					os.Exit(1)
@@ -172,6 +176,7 @@ func main() {
 				if !settings.RunHook("after.scale") {
 					os.Exit(1)
 				}
+				return nil
 			},
 		},
 		{
@@ -179,7 +184,7 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Restart containers",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.restart") {
 					os.Exit(1)
@@ -194,6 +199,7 @@ func main() {
 				if !settings.RunHook("after.restart") {
 					os.Exit(1)
 				}
+				return nil
 			},
 		},
 		{
@@ -201,7 +207,7 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Stop running containers",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.stop") {
 					os.Exit(1)
@@ -214,6 +220,8 @@ func main() {
 				if !settings.RunHook("after.stop") {
 					os.Exit(1)
 				}
+
+				return nil
 			},
 		},
 		{
@@ -221,7 +229,7 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Kill running containers using SIGKILL or a specified signal",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.kill") {
 					os.Exit(1)
@@ -234,6 +242,7 @@ func main() {
 				if !settings.RunHook("after.kill") {
 					os.Exit(1)
 				}
+				return nil
 			},
 		},
 		{
@@ -241,7 +250,7 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Remove stopped containers",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.rm") {
 					os.Exit(1)
@@ -254,6 +263,7 @@ func main() {
 				if !settings.RunHook("after.rm") {
 					os.Exit(1)
 				}
+				return nil
 			},
 		},
 		{
@@ -261,13 +271,14 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Show container status",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if err := settings.CapitanPs(c.Args()); err != nil {
 					Error.Println("Ps failed:", err)
 					os.Exit(1)
 				}
 
+				return nil
 			},
 		},
 		{
@@ -275,20 +286,20 @@ func main() {
 			Aliases:         []string{},
 			Usage:           "Show container ip addresses",
 			SkipFlagParsing: true,
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if err := settings.ContainerList.CapitanIP(); err != nil {
 					Error.Println("IP failed:", err)
 					os.Exit(1)
 				}
-
+				return nil
 			},
 		},
 		{
 			Name:    "build",
 			Aliases: []string{},
 			Usage:   "Build any containers with 'build' flag set",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if !settings.RunHook("before.build") {
 					os.Exit(1)
@@ -300,60 +311,61 @@ func main() {
 				if !settings.RunHook("after.build") {
 					os.Exit(1)
 				}
+				return nil
 			},
 		},
 		{
 			Name:    "pull",
 			Aliases: []string{},
 			Usage:   "Pull all images defined in project",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if err := settings.ContainerList.CapitanPull(dryRun); err != nil {
 					Error.Println("Pull failed:", err)
 					os.Exit(1)
 				}
-
+				return nil
 			},
 		},
 		{
 			Name:    "logs",
 			Aliases: []string{},
 			Usage:   "stream container logs",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				combined := append(settings.ContainerList, settings.ContainerCleanupList...)
 				if err := combined.CapitanLogs(); err != nil {
 					Error.Println("Logs failed:", err)
 					os.Exit(1)
 				}
-
+				return nil
 			},
 		},
 		{
 			Name:    "stats",
 			Aliases: []string{},
 			Usage:   "stream stats for all containers in project",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				combined := append(settings.ContainerList, settings.ContainerCleanupList...)
 				if err := combined.CapitanStats(); err != nil {
 					Error.Println("Stats failed:", err)
 					os.Exit(1)
 				}
-
+				return nil
 			},
 		},
 		{
 			Name:    "show",
 			Aliases: []string{},
 			Usage:   "Prints config as interpreted by Capitan",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				settings := getSettings()
 				if err := settings.CapitanShow(); err != nil {
 					Error.Println("Show failed:", err)
 					os.Exit(1)
 				}
-
+				return nil
 			},
 		},
 	}
